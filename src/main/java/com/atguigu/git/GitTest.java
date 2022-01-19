8049b873620369461845e43ba7745b6274246dea
root <root@qq.com>
2022-01-19 11:47:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author:小明
+ * @DateTime:2022/1/19 11:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
